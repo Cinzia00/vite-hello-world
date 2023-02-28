857d25a301bebc3439be89ea9248fa41954c7d9a
Cinzia Pulvirenti <cinzia2092@hotmail.it>
2023-02-28 19:18:17 +0100

    Feat creato components

---
+++ vite-hello-world/src/App.vue
@@ -0,0 +1,43 @@
+<script>
+import appTitle from './components/appTitle.vue'
+
+export default {
+  components: {
+    appTitle
+  },
+  data() {
+    return {
+      title: 'La mia prima app con Vite'
+    }
+  },
+}
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+  <h1>{{ title }}</h1>
+  <appTitle></appTitle>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
